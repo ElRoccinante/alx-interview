6a90392ac094da0f0ed54c7e9aa15d21c76ba681
ElRoccinante <alaechouiye@gmail.com>
2024-07-03 15:50:40 +0000

    j

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,53 @@
 #!/usr/bin/python3
-"""
-0x01. Lockboxes
-"""
+"""Lockboxes is it locked"""
+
+
+class Keys:
+    """keys class"""
+
+    def __init__(self, numOfBoxes: int):
+        """init function"""
+        self.keys = {}
+        for i in range(numOfBoxes):
+            self.keys[f'{i}'] = True if i == 0 else False
+
+
+class Boxes:
+    """boxes info class"""
+    Locked = []
+
+
+class Box:
+    """box class"""
+
+    def __init__(self, keysList: list, idx: int, k: Keys):
+        """init function"""
+        self._k = k
+        self._keysList = keysList
+        self._idx = idx
+        if str(idx) in k.keys and k.keys[str(idx)] is True:
+            for key in keysList:
+                k.keys[str(key)] = True
+        else:
+            Boxes.Locked.append(self)
+
+    def update(self):
+        """update function"""
+        if str(self._idx) in self._k.keys and\
+                self._k.keys[str(self._idx)] is True:
+            for key in self._keysList:
+                self._k.keys[str(key)] = True
 
 
 def canUnlockAll(boxes):
-    """A function that determines if all the boxes can be opened."""
-    unlocked_boxes = {0}  # Start with box 0 unlocked
-    prev_count = 0
-
-    while len(unlocked_boxes) > prev_count:
-        prev_count = len(unlocked_boxes)
-
-        for box in unlocked_boxes.copy():
-            unlocked_boxes.update(boxes[box])
-
-    return len(unlocked_boxes) == len(boxes)
+    """canUnlockAll function"""
+    size = len(boxes)
+    k = Keys(size)
+    for i in range(size):
+        Box(boxes[i], i, k)
+    for box in Boxes.Locked:
+        box.update()
+    for val in k.keys.values():
+        if not val:
+            return False
+    return True
